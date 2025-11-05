ef5a48d63273af58ffbdbec149b39d67e70cfbb4
Andrew Pettey <andrew@pettey.me>
2025-11-05 23:24:14 +0100

    chore: fmt

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,5 +1,5 @@
 // This file is used during local development
 // In production, this file is generated at runtime by docker-entrypoint.sh
 window.__RUNTIME_CONFIG__ = {
-  API_BASE_URL: 'http://localhost:3000'
+  API_BASE_URL: 'http://localhost:3000',
 };
